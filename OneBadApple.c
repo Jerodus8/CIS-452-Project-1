af2bf3f8eedef3b3145d665a2b9140bfd60a428c
Synjin Shanley <shanlesy@mail.gvsu.edu>
2025-10-04 19:13:11 -0400

    add main project file

---
+++ OneBadApple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+
+int main(void) {
+    return 0;
+}
